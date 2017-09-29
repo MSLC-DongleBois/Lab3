e2d51c5362ccf0ccca5d5e265537952f0467000a
Austin Chen <chenas@smu.edu>
2017-09-29 10:04:38 -0500

    Kind of game

--- Commotion/GameScene.swift
+++ Commotion/GameScene.swift
@@ -11,10 +11,13 @@ import SpriteKit
 import CoreMotion
 
 class GameScene: SKScene {
+    
+    
 
     
     // MARK: Raw Motion Functions
     let motion = CMMotionManager()
+    
     func startMotionUpdates(){
         // some internal inconsistency here: we need to ask the device manager for device
         
@@ -26,81 +29,116 @@ class GameScene: SKScene {
     
     func handleMotion(_ motionData:CMDeviceMotion?, error:Error?){
         if let gravity = motionData?.gravity {
-            self.physicsWorld.gravity = CGVector(dx: CGFloat(9.8*gravity.x), dy: CGFloat(9.8*gravity.y))
+            self.physicsWorld.gravity = CGVector(dx: CGFloat(20.8*gravity.x), dy: CGFloat(20.8*gravity.y))
         }
     }
+    
+
     
     // MARK: View Hierarchy Functions
     override func didMove(to view: SKView) {
         
-        backgroundColor = SKColor.white
+        //backgroundColor = SKColor.green
+        let background = SKSpriteNode(imageNamed: "background_leaf.jpg")
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        background.zPosition = -1
+        addChild(background)
+        
+        //backgroundColor = SKColor.white
         
         // start motion for gravity
         self.startMotionUpdates()
         
         // make sides to the screen
-        self.addSidesAndTop()
+        self.addGameBoundaries()
         
-        // add some stationary blocks
-        self.addStaticBlockAtPoint(CGPoint(x: size.width * 0.1, y: size.height * 0.25))
-        self.addStaticBlockAtPoint(CGPoint(x: size.width * 0.9, y: size.height * 0.25))
-        
-        // add a spinning block
-        self.addBlockAtPoint(CGPoint(x: size.width * 0.5, y: size.height * 0.35))
-        
-        self.addSprite()
+        self.addBoi()
+        self.addObstacle(CGPoint(x: size.width/2, y: size.height * 0.33))
+        self.addObstacle(CGPoint(x: size.width/2, y: size.height * 0.66))
+        self.addApple()
+        self.addSpinner()
     }
+    
     
     // MARK: Create Sprites Functions
-    func addSprite(){
-        let spriteA = SKSpriteNode(imageNamed: "sprite") // this is literally a sprite bottle... 😎
+    func addBoi(){
         
-        spriteA.size = CGSize(width:size.width*0.1,height:size.height * 0.1)
+        let boi = SKSpriteNode(imageNamed: "kellen.png")
         
-        spriteA.position = CGPoint(x: size.width * random(min: CGFloat(0.1), max: CGFloat(0.9)), y: size.height * 0.75)
+        boi.size = CGSize(width:size.width*0.13,height:size.height * 0.11492169)
         
-        spriteA.physicsBody = SKPhysicsBody(rectangleOf:spriteA.size)
-        spriteA.physicsBody?.restitution = random(min: CGFloat(1.0), max: CGFloat(1.5))
-        spriteA.physicsBody?.isDynamic = true
+        boi.position = CGPoint(x: size.width/2, y: size.height * 0.18)
         
-        self.addChild(spriteA)
-    }
-    
-    func addBlockAtPoint(_ point:CGPoint){
-        let 🔲 = SKSpriteNode()
-        
-        🔲.color = UIColor.red
-        🔲.size = CGSize(width:size.width*0.15,height:size.height * 0.05)
-        🔲.position = point
-        
-        🔲.physicsBody = SKPhysicsBody(rectangleOf:🔲.size)
-        🔲.physicsBody?.isDynamic = true
-        🔲.physicsBody?.pinned = true
-        
-        self.addChild(🔲)
+        boi.physicsBody = SKPhysicsBody(rectangleOf:boi.size)
+        //boi.physicsBody?.restitution = random(min: CGFloat(1.0), max: CGFloat(1.5))
+        boi.physicsBody?.restitution = 0.15
 
+        boi.physicsBody?.isDynamic = true
+        boi.zPosition = 1
+        
+        self.addChild(boi)
     }
     
-    func addStaticBlockAtPoint(_ point:CGPoint){
-        let 🔲 = SKSpriteNode()
+    func addObstacle(_ point:CGPoint){
         
-        🔲.color = UIColor.red
-        🔲.size = CGSize(width:size.width*0.1,height:size.height * 0.05)
-        🔲.position = point
+        let wall = SKSpriteNode(imageNamed: "wall.jpg")
         
-        🔲.physicsBody = SKPhysicsBody(rectangleOf:🔲.size)
-        🔲.physicsBody?.isDynamic = true
-        🔲.physicsBody?.pinned = true
-        🔲.physicsBody?.allowsRotation = false
+        wall.size = CGSize(width:size.width*0.4,height: size.height * 0.07 )
         
-        self.addChild(🔲)
+        wall.position = point
+        
+        wall.physicsBody = SKPhysicsBody(rectangleOf:wall.size)
+        wall.physicsBody?.restitution = random(min: CGFloat(1.0), max: CGFloat(1.5))
+        wall.physicsBody?.restitution = 1
+        
+        wall.physicsBody?.isDynamic = false
+        wall.zPosition = 1
+        
+        self.addChild(wall)
+    }
+    
+    func addSpinner(){
+        
+        let boi = SKSpriteNode(imageNamed: "spinner")
+        
+        boi.size = CGSize(width:size.width*0.15,height:size.height * 0.09)
+        
+        boi.position = CGPoint(x: size.width/3, y: size.height * 0.18)
+        
+        boi.physicsBody = SKPhysicsBody(circleOfRadius: boi.size.width/2)
+        //boi.physicsBody?.restitution = random(min: CGFloat(1.0), max: CGFloat(1.5))
+        boi.physicsBody?.restitution = 0.15
+        
+        boi.physicsBody?.isDynamic = true
+        boi.zPosition = 1
+        
+        self.addChild(boi)
+    }
+    
+    func addApple(){
+        
+        let boi = SKSpriteNode(imageNamed: "apple")
+        
+        boi.size = CGSize(width:size.width*0.11,height:size.height * 0.07)
+        
+        boi.position = CGPoint(x: size.width/2, y: size.height * 0.75)
+        
+        boi.physicsBody = SKPhysicsBody(rectangleOf:boi.size)
+        //boi.physicsBody?.restitution = random(min: CGFloat(1.0), max: CGFloat(1.5))
+        boi.physicsBody?.restitution = 0.9
+        
+        boi.physicsBody?.isDynamic = true
+        boi.zPosition = 1
+        
+        self.addChild(boi)
         
     }
     
-    func addSidesAndTop(){
+    func addGameBoundaries(){
         let left = SKSpriteNode()
         let right = SKSpriteNode()
         let top = SKSpriteNode()
+        let bottom = SKSpriteNode()
         
         left.size = CGSize(width:size.width*0.1,height:size.height)
         left.position = CGPoint(x:0, y:size.height*0.5)
@@ -111,19 +149,24 @@ class GameScene: SKScene {
         top.size = CGSize(width:size.width,height:size.height*0.1)
         top.position = CGPoint(x:size.width*0.5, y:size.height)
         
-        for obj in [left,right,top]{
-            obj.color = UIColor.red
+        bottom.size = CGSize(width:size.width,height:size.height*0.1)
+        bottom.position = CGPoint(x:size.width*0.5, y:0)
+        
+        for obj in [left,right,top,bottom]{
+            obj.color = UIColor.brown
             obj.physicsBody = SKPhysicsBody(rectangleOf:obj.size)
             obj.physicsBody?.isDynamic = true
             obj.physicsBody?.pinned = true
             obj.physicsBody?.allowsRotation = false
             self.addChild(obj)
         }
+        
+        
     }
     
     // MARK: UI Delegate Functions
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.addSprite()
+        self.addBoi()
     }
     
     // MARK: Utility Functions (thanks ray wenderlich!)
